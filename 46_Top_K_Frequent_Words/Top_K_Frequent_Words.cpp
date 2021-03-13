1afa0a7fea4b2676d99810d16978c584c0fcfdd4
Chun-Chi Huang <ryancc.huang0729@gmail.com>
2021-03-13 09:07:46 -0800

    Modify for Top K Frequent Words

--- 46_Top_K_Frequent_Words/Top_K_Frequent_Words.cpp
+++ 46_Top_K_Frequent_Words/Top_K_Frequent_Words.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -37,6 +38,43 @@ vector<string> topKFrequent(vector<string> &words, int k)
     return res;
 }
 
+struct Comp
+{
+    bool operator()(pair<int, string> &a, pair<int, string> &b)
+    {
+        return a.first == b.first ? a.second < b.second : a.first > b.first;
+                                    //descreasing(less)   // increasing(greater)
+    }
+};
+
+vector<string> topKFrequent2(vector<string> &words, int k)
+{
+    unordered_map<string, int> umap;
+    priority_queue<pair<int, string>, vector<pair<int, string>>, Comp> pq;
+    vector<string> res;
+
+    for (auto word : words)
+        umap[word]++;
+
+    for (auto e : umap)
+    {
+        pq.push(make_pair(e.second, e.first));
+
+        if (pq.size() > k)
+            pq.pop();
+    }
+
+    while (!pq.empty())
+    {
+        res.push_back(pq.top().second);
+        pq.pop();
+    }
+
+    reverse(res.begin(), res.end());
+
+    return res;
+}
+
 int main()
 {
     vector<string> words = {"i", "love", "leetcode", "i", "love", "coding"};
@@ -45,4 +83,8 @@ int main()
     vector<string> ans = topKFrequent(words, k);
 
     printVector(ans);
+
+    vector<string> ans2 = topKFrequent2(words, k);
+
+    printVector(ans2);
 }
